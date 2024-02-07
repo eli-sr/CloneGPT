1dd05d1e5e9c84e3750c9e1ffc7dd6863ae8ef5f
eli <elzr.sr@gmail.com>
2024-02-07 01:40:19 +0100

    Home page design

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
+import Image from 'next/image'
 import Header from './components/Header'
 
 export default function Home () {
   return (
     <>
       <Header />
-      <main>
-        <div>
-          <h1 className='text-xl font-semibold'>How can I help you today?</h1>
+      <main className='flex flex-col w-full h-full'>
+        <div className='flex flex-col items-center justify-center grow'>
+          <figure className='p-2 mb-4 bg-white rounded-full'>
+            <Image
+              src='https://www.svgrepo.com/show/306500/openai.svg'
+              width={30}
+              height={30}
+              alt='OpenAI logo'
+            />
+          </figure>
+          <span className='text-2xl font-semibold'>How can I help you today?</span>
         </div>
       </main>
     </>
