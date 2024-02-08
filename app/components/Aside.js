ebab884f99d21c4a29ab7c22c83a1ec6f2da1dac
eli <elzr.sr@gmail.com>
2024-02-08 17:13:48 +0100

    New chat button sticky

--- app/components/Aside.js
+++ app/components/Aside.js
@@ -8,22 +8,24 @@ import { useEffect, useState } from 'react'
 
 function NewChatButton () {
   return (
-    <Button
-      fullWidth
-      variant='light'
-      className='justify-start px-2 font-semibold text-white rounded-lg mb-7'
-    >
-      <figure className='p-1 bg-white rounded-full'>
-        <Image
-          src='https://www.svgrepo.com/show/306500/openai.svg'
-          width={20}
-          height={20}
-          alt='OpenAI logo'
-        />
-      </figure>
-      <span className='text-left grow'>New chat</span>
-      ico
-    </Button>
+    <div className='sticky top-0 z-10 mb-7'>
+      <Button
+        fullWidth
+        variant='light'
+        className='justify-start px-2 font-semibold text-white rounded-lg'
+      >
+        <figure className='p-1 bg-white rounded-full'>
+          <Image
+            src='https://www.svgrepo.com/show/306500/openai.svg'
+            width={20}
+            height={20}
+            alt='OpenAI logo'
+          />
+        </figure>
+        <span className='text-left grow'>New chat</span>
+        ico
+      </Button>
+    </div>
   )
 }
 
@@ -95,7 +97,7 @@ function ChatList () {
 
 export default function Aside () {
   return (
-    <aside className='bg-black min-w-[260px] w-[260px] p-3 overflow-scroll'>
+    <aside className='bg-black min-w-[260px] w-[260px] p-3 overflow-scroll relative'>
       <NewChatButton />
       <ChatList />
     </aside>
