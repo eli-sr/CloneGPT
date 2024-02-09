09720a1c93f688c079e910cb49809cfb242a686f
eli <elzr.sr@gmail.com>
2024-02-09 17:53:44 +0100

    Separating page into page and layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Aside from './components/Aside.js'
+import Header from './components/Header'
+import ChatBar from './components/ChatBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +16,15 @@ export default function RootLayout ({ children }) {
     <html lang='en'>
       <body className={`${inter.className} flex w-full h-screen`}>
         <Aside />
-        {children}
+        <div className='flex flex-col grow'>
+          <Header />
+          <main className='flex items-center justify-center grow'>
+            <div className='flex flex-col w-full h-full lg:max-w-2xl xl:max-w-3xl'>
+              {children}
+              <ChatBar />
+            </div>
+          </main>
+        </div>
       </body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -1,27 +1,17 @@
 import Image from 'next/image'
-import Header from './components/Header'
-import ChatBar from './components/ChatBar'
 
 export default function Home () {
   return (
-    <div className='flex flex-col grow'>
-      <Header />
-      <main className='flex items-center justify-center grow'>
-        <div className='flex flex-col w-full h-full lg:max-w-2xl xl:max-w-3xl'>
-          <div className='flex flex-col items-center justify-center grow'>
-            <figure className='p-2 mb-4 bg-white rounded-full'>
-              <Image
-                src='https://www.svgrepo.com/show/306500/openai.svg'
-                width={30}
-                height={30}
-                alt='OpenAI logo'
-              />
-            </figure>
-            <span className='text-2xl font-semibold'>How can I help you today?</span>
-          </div>
-          <ChatBar />
-        </div>
-      </main>
+    <div className='flex flex-col items-center justify-center grow'>
+      <figure className='p-2 mb-4 bg-white rounded-full'>
+        <Image
+          src='https://www.svgrepo.com/show/306500/openai.svg'
+          width={30}
+          height={30}
+          alt='OpenAI logo'
+        />
+      </figure>
+      <span className='text-2xl font-semibold'>How can I help you today?</span>
     </div>
   )
 }
